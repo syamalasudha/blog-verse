a9f9ad5c26de809f8daca081a9e609e0adb01e52
syamalasudha <syamalasudha139@gmail.com>
2025-09-06 12:33:51 +0530

    Learned basics of js

--- practice.js
+++ practice.js
@@ -1,1 +1,29 @@
 console.log("Hello World,Syamala Sudha")
+
+let name="Syamala";
+console.log(name)
+
+let num=99;
+console.log(num)
+
+let isLogged=false;
+console.log(isLogged)
+let x;
+console.log(x)
+let y=null;
+console.log(y)
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof isLogged)
+console.log(typeof x)
+console.log(typeof y)
+let arr = [1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+let person= {
+    name:"Syamala",
+    age:19
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
