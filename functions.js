a308fa0fc3cb3094753f237bb8f849d050657a3b
syamalasudha <syamalasudha139@gmail.com>
2025-09-06 15:54:27 +0530

    Functions completed

---
+++ functions.js
@@ -0,0 +1,22 @@
+function greet(){
+    console.log("Welcome to Dev Astra")
+}
+greet()
+function add(x,y){
+    return x+y;
+}
+let result=add(3,5);
+console.log(result)
+const pi=3.14
+console.log(pi)
+
+const multiply=function(x,y){
+    return x*y;
+}
+console.log(multiply(5,3))
+const square=(x)=>x*x;
+console.log(square(5))
+const subract=(x,y)=>{
+    return x-y;
+}
+console.log(subract(20,15))
